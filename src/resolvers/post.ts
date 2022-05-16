8daa2f793d7bc612e47dd378c4184d1051ab23b4
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2022-05-16 16:32:15 -0700

    Crud with microORM for a basic entity. I'm still very curious why we're using graphQL but whatever

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,5 +1,6 @@
+import { FilterQuery } from '@mikro-orm/core/typings';
 import { MyContext } from 'src/types';
-import { Ctx, Query, Resolver } from 'type-graphql';
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from 'type-graphql';
 import { Post } from '../entities/Post';
 
 // When using typeGraphQL we need to set types twice.
@@ -8,5 +9,58 @@ export class PostResolver {
   @Query(() => [Post])
   posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(
+    @Arg('id', () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { _id: id });
+  }
+
+  // Mutations are for updating inserting deleting
+  @Mutation(() => Post, { nullable: true })
+  async createPost(
+    @Arg('title') title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = em.create(Post, { title } as Post);
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg('id') id: number,
+    @Arg('title') title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { _id: id });
+    if (!post) {
+      return null;
+    }
+
+    // May want to include the line below if we care about untitled earlier posts. I feel like this was actually a bad case for ben to add.
+
+    // if (typeof title !== 'undefined') {
+    // }
+    post.title = title;
+    await em.persistAndFlush(post);
+
+    return post;
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg('id') id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Boolean> {
+    try {
+      await em.nativeDelete(Post, { _id: id });
+      return true;
+    } catch (err) {
+      return false;
+    }
   }
 }
